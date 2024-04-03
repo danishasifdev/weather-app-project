35ab013e1680d78cdbfc859c1811d841f1923b1b
Danish <danishdevv@users.noreply.github.com>
2024-04-04 04:04:21 +0500

    add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,146 @@
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div>Weather App</div>;
+  const [location, setLocation] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [displayLocation, setDisplayLocation] = useState("");
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      if (location.length < 2) return setWeather({});
+
+      try {
+        setIsLoading(true);
+
+        const geoRes = await fetch(
+          `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
+        );
+        const geoData = await geoRes.json();
+
+        if (!geoData.results) throw new Error("Location not found");
+
+        const { latitude, longitude, timezone, name, country_code } =
+          geoData.results[0];
+
+        setDisplayLocation(`${name} ${convertToFlag(country_code)}`);
+
+        const weatherRes = await fetch(
+          `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min,wind_speed_10m_max`
+        );
+        const weatherData = await weatherRes.json();
+        setWeather(weatherData.daily);
+        console.log(weatherData.daily);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchWeather();
+  }, [location]);
+
+  const convertToFlag = (countryCode) => {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split("")
+      .map((char) => 127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+  };
+
+  return (
+    <div>
+      <h1>Classy Weather</h1>
+      <Input location={location} onChangeLocation={setLocation} />
+
+      {isLoading && <p>Loading...</p>}
+
+      {weather.weathercode && (
+        <Weather weather={weather} location={displayLocation} />
+      )}
+    </div>
+  );
+}
+
+function Input({ location, onChangeLocation }) {
+  return (
+    <div>
+      <input
+        type='text'
+        placeholder='Search from location...'
+        value={location}
+        onChange={(e) => onChangeLocation(e.target.value)}
+      />
+    </div>
+  );
+}
+
+function Weather({ weather, location }) {
+  const {
+    temperature_2m_max: max,
+    temperature_2m_min: min,
+    time: dates,
+    weathercode: codes,
+    wind_speed_10m_max: windspeed,
+  } = weather;
+
+  return (
+    <div>
+      <h2>Weather {location}</h2>
+      <ul>
+        {dates.map((date, i) => (
+          <Day
+            date={date}
+            max={max[i]}
+            min={min[i]}
+            code={codes[i]}
+            key={date}
+            isToday={i === 0}
+            speed={windspeed[i]}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function formatDay(dateStr) {
+  return new Intl.DateTimeFormat("en", {
+    weekday: "short",
+  }).format(new Date(dateStr));
+}
+
+function getWeatherIcon(wmoCode) {
+  const icons = new Map([
+    [[0], "☀️"],
+    [[1], "🌤"],
+    [[2], "⛅️"],
+    [[3], "☁️"],
+    [[45, 48], "🌫"],
+    [[51, 56, 61, 66, 80], "🌦"],
+    [[53, 55, 63, 65, 57, 67, 81, 82], "🌧"],
+    [[71, 73, 75, 77, 85, 86], "🌨"],
+    [[95], "🌩"],
+    [[96, 99], "⛈"],
+  ]);
+  const arr = [...icons.keys()].find((key) => key.includes(wmoCode));
+  if (!arr) return "NOT FOUND";
+  return icons.get(arr);
+}
+
+function Day({ date, max, min, code, isToday, speed }) {
+  return (
+    <li>
+      <span>{getWeatherIcon(code)}</span>
+      <p>{isToday ? "Today" : formatDay(date)}</p>
+      <p>
+        {Math.floor(min)}&deg; &mdash; <strong>{Math.ceil(max)}&deg;</strong>
+      </p>
+
+      <p>Wind: {speed} km/h</p>
+    </li>
+  );
 }
 
 export default App;
